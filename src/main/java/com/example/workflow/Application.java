156a353d92d029f0ada196a7b470030564e98ba5
Rajesh Shenoy <rshenoy@bp-3.com>
2024-02-27 10:58:29 +0000

    first commit

---
+++ src/main/java/com/example/workflow/Application.java
@@ -0,0 +1,26 @@
+package com.example.workflow;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.RuntimeService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+  public static void main(String... args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+   RuntimeService rs = ProcessEngines.getDefaultProcessEngine().getRuntimeService();
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("inputArray", new Integer[]{5, 23, 42});
+    rs.startProcessInstanceByKey("my-script-task-process", variables);
+
+  }
+}
